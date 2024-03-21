54f2d1e282c48255c71eb260f642d14f4104ced4
marshfellow42 <123888466+marshfellow42@users.noreply.github.com>
2024-03-21 20:16:44 -0300

    Adding another wait time for the website to enter and change some variables from int to float

--- script.py
+++ script.py
@@ -6,9 +6,13 @@ url = input("What's your GitHub profile link?: ")
 
 refresh_limit = int(input("How many times should it refresh?: "))
 
-wait_time = int(input("How many seconds should it take to refresh?: "))
+website_load_time = float(input("How long should it take for the website to fully load?: "))
+
+wait_time = float(input("How many seconds should it take to refresh?: "))
 
 webbrowser.open(url, new=0)
+
+time.sleep(website_load_time)
 
 for i in range(refresh_limit):
     time.sleep(wait_time)
